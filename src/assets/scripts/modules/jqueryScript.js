179947881403061465db5e21a4e863b9ecdec950
Alex_Volkov <alexvolkov72305@yandex.ru>
2020-04-05 21:50:12 +0300

    21:49 05.04.2020

--- src/assets/scripts/modules/jqueryScript.js
+++ src/assets/scripts/modules/jqueryScript.js
@@ -11,6 +11,7 @@ $(document).ready(function(){
 			$(this).hide();
 			$('.close_block').show();
 			$(".homepage").css("left","0");
+			$("body").addClass("overflow-hidden");
 			
 		});
 		$('.close_block').on('click', function () {
@@ -18,7 +19,109 @@ $(document).ready(function(){
 			$(this).hide();
 			$('.open_block').show();
 			$(".homepage").css("left","-100%");
+			$("body").removeClass("overflow-hidden");
 		});
-	}	
+	}
+
+	/*if ($('.tabs').length) {
+			$('.tab_item-1').on('click', function () {
+				$(this).addClass('active');
+				$('.tab_item-2').removeClass('active');
+				$('.tab_item-3').removeClass('active');
+				$('.tab_item-4').removeClass('active');
+				$('.tab_item-5').removeClass('active');
+				$('.tab_item-6').removeClass('active');
+				$('.tab_content-1').show();
+				$('.tab_content-2').hide();
+				$('.tab_content-3').hide();
+				$('.tab_content-4').hide();
+				$('.tab_content-5').hide();
+				$('.tab_content-6').hide();
+			});
+			$('.tab_item-2').on('click', function () {
+				$(this).addClass('active');
+				$('.tab_item-1').removeClass('active');
+				$('.tab_item-3').removeClass('active');
+				$('.tab_item-4').removeClass('active');
+				$('.tab_item-5').removeClass('active');
+				$('.tab_item-6').removeClass('active');
+				$('.tab_content-1').hide();
+				$('.tab_content-2').show();
+				$('.tab_content-3').hide();
+				$('.tab_content-4').hide();
+				$('.tab_content-5').hide();
+				$('.tab_content-6').hide();
+			});
+			$('.tab_item-3').on('click', function () {
+				$(this).addClass('active');
+				$('.tab_item-2').removeClass('active');
+				$('.tab_item-1').removeClass('active');
+				$('.tab_item-4').removeClass('active');
+				$('.tab_item-5').removeClass('active');
+				$('.tab_item-6').removeClass('active');
+				$('.tab_content-1').hide();
+				$('.tab_content-2').hide();
+				$('.tab_content-3').show();
+				$('.tab_content-4').hide();
+				$('.tab_content-5').hide();
+				$('.tab_content-6').hide();
+			});
+			$('.tab_item-4').on('click', function () {
+				$(this).addClass('active');
+				$('.tab_item-2').removeClass('active');
+				$('.tab_item-3').removeClass('active');
+				$('.tab_item-1').removeClass('active');
+				$('.tab_item-5').removeClass('active');
+				$('.tab_item-6').removeClass('active');
+				$('.tab_content-1').hide();
+				$('.tab_content-2').hide();
+				$('.tab_content-3').hide();
+				$('.tab_content-4').show();
+				$('.tab_content-5').hide();
+				$('.tab_content-6').hide();
+			});
+			$('.tab_item-5').on('click', function () {
+				$(this).addClass('active');
+				$('.tab_item-2').removeClass('active');
+				$('.tab_item-3').removeClass('active');
+				$('.tab_item-4').removeClass('active');
+				$('.tab_item-1').removeClass('active');
+				$('.tab_item-6').removeClass('active');
+				$('.tab_content-1').hide();
+				$('.tab_content-2').hide();
+				$('.tab_content-3').hide();
+				$('.tab_content-4').hide();
+				$('.tab_content-5').show();
+				$('.tab_content-6').hide();
+			});
+			$('.tab_item-6').on('click', function () {
+				$(this).addClass('active');
+				$('.tab_item-2').removeClass('active');
+				$('.tab_item-3').removeClass('active');
+				$('.tab_item-4').removeClass('active');
+				$('.tab_item-5').removeClass('active');
+				$('.tab_item-1').removeClass('active');
+				$('.tab_content-1').hide();
+				$('.tab_content-2').hide();
+				$('.tab_content-3').hide();
+				$('.tab_content-4').hide();
+				$('.tab_content-5').hide();
+				$('.tab_content-6').show();
+			});
+	}*/
+
+	if ($('.slider_card').length) {
+		$('.slider_card').slick({
+	   	arrows: false,
+	   	dots: true
+	   });
+	}
+
 });
 
+/*$('.testimonials_carousel').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+var tabs_container = $('.testimonial_tab_container');
+
+tabs_container.find($('.tab-item')).hide();
+tabs_container.find($('.tab-' + nextSlide)).show();
+});*/
